b697c41e699ea94344e607506c822a1f460474e3
finnsueess3-sudo <finn.sueess3@gmail.com>
2025-11-24 10:42:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,23 +6,27 @@ const io = require('socket.io')(http);
 app.use(express.static('public'));
 
 const players = {};
+let projectiles = [];
 
 io.on('connection', (socket) => {
   console.log(`Spieler verbunden: ${socket.id}`);
 
+  // Spieler initialisieren
   players[socket.id] = {
     x: 400,
     y: 300,
     classKey: 'mage',
     hp: 100,
     color: '#f60',
+    dir: 0,
     abilities: { q:0, e:0, x:0 },
-    dir:0
+    r: 14
   };
 
-  socket.emit('currentPlayers', players);
+  socket.emit('currentPlayers', players, projectiles);
   socket.broadcast.emit('newPlayer', { id: socket.id, ...players[socket.id] });
 
+  // Bewegung
   socket.on('move', data => {
     if(players[socket.id]){
       players[socket.id].x = data.x;
@@ -32,12 +36,55 @@ io.on('connection', (socket) => {
     }
   });
 
-  socket.on('castSpell', data => io.emit('spellCast', { id: socket.id, ...data }));
+  // Spell cast
+  socket.on('castSpell', data => {
+    if(!players[socket.id]) return;
+    const p = {
+      id: socket.id,
+      type: data.type,
+      x: data.x,
+      y: data.y,
+      vx: data.vx || 0,
+      vy: data.vy || 0,
+      life: data.life || 1,
+      dmg: data.dmg || 1,
+      r: data.r || 6
+    };
+    projectiles.push(p);
+    io.emit('spellCast', p);
+  });
 
   socket.on('disconnect', () => {
     delete players[socket.id];
     io.emit('playerDisconnected', socket.id);
   });
 });
+
+// Server Tick: Update Projektile & Schaden
+setInterval(()=>{
+  const dt = 1/60;
+
+  for(let i=projectiles.length-1;i>=0;i--){
+    const p = projectiles[i];
+    p.x += (p.vx||0)*dt;
+    p.y += (p.vy||0)*dt;
+    if(p.life) p.life -= dt;
+    if(p.life <=0) { projectiles.splice(i,1); continue; }
+
+    // Schaden prüfen
+    for(const id in players){
+      if(id === p.id) continue; // Eigene nicht treffen
+      const pl = players[id];
+      const d = Math.hypot(pl.x-p.x, pl.y-p.y);
+      if(d < pl.r + p.r){
+        pl.hp -= p.dmg;
+        if(pl.hp<0) pl.hp=0;
+        io.emit('playerHit', { id, hp: pl.hp });
+        projectiles.splice(i,1);
+        break;
+      }
+    }
+  }
+}, 1000/60);
 
 http.listen(3000, () => console.log('Server läuft auf http://localhost:3000'));
