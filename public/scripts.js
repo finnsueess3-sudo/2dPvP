29f41e71ad63864adf47a24f4a9a5ddc33d3660c
finnsueess3-sudo <finn.sueess3@gmail.com>
2025-11-24 10:43:12 +0100

    Update scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -1,12 +1,17 @@
 const socket = io();
 const otherPlayers = {};
-socket.on('currentPlayers', players => {
+let projectiles = [];
+
+socket.on('currentPlayers', (players, serverProjectiles) => {
   for(const id in players) if(id !== socket.id) otherPlayers[id] = players[id];
+  projectiles = serverProjectiles;
 });
-socket.on('newPlayer', p => otherPlayers[p.id]=p);
+
+socket.on('newPlayer', p => otherPlayers[p.id] = p);
 socket.on('playerMoved', data => { if(otherPlayers[data.id]) Object.assign(otherPlayers[data.id], data); });
 socket.on('playerDisconnected', id => delete otherPlayers[id]);
-socket.on('spellCast', data => { /* hier Spell-Visualisierung */ });
+socket.on('spellCast', p => projectiles.push(p));
+socket.on('playerHit', data => { if(data.id===socket.id) player.hp = data.hp; });
 
 function sendMove(x,y,dir){ socket.emit('move',{x,y,dir}); }
-function sendSpell(type,x,y,dir){ socket.emit('castSpell',{type,x,y,dir}); }
+function sendSpell(type,x,y,vx,vy,life,dmg,r){ socket.emit('castSpell',{type,x,y,vx,vy,life,dmg,r}); }
